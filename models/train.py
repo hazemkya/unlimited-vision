398d3248aa991f69491eb525f48dc7e5c8dc76c8
Hazem <hazemkya@gmail.com>
2022-04-10 22:39:36 +0300

    bug fixes

---
+++ models/train.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+import time
+
+optimizer = tf.keras.optimizers.Adam()
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+
+# adding this in a separate cell because if you run the training cell
+# many times, the loss_plot array will be reset
+loss_plot = []
+
+
+@tf.function
+def train_step(img_tensor, target, decoder, encoder, word_to_index):
+    loss = 0
+
+    # initializing the hidden state for each batch
+    # because the captions are not related from image to image
+    hidden = decoder.reset_state(batch_size=target.shape[0])
+
+    dec_input = tf.expand_dims([word_to_index('<start>')] * target.shape[0], 1)
+
+    with tf.GradientTape() as tape:
+        features = encoder(img_tensor)
+
+        for i in range(1, target.shape[1]):
+            # passing the features through the decoder
+            predictions, hidden, _ = decoder(dec_input, features, hidden)
+
+            loss += loss_function(target[:, i], predictions)
+
+            # using teacher forcing
+            dec_input = tf.expand_dims(target[:, i], 1)
+
+    total_loss = (loss / int(target.shape[1]))
+
+    trainable_variables = encoder.trainable_variables + decoder.trainable_variables
+
+    gradients = tape.gradient(loss, trainable_variables)
+
+    optimizer.apply_gradients(zip(gradients, trainable_variables))
+
+    return loss, total_loss
+
+
+def loss_function(real, pred):
+    mask = tf.math.logical_not(tf.math.equal(real, 0))
+    loss_ = loss_object(real, pred)
+
+    mask = tf.cast(mask, dtype=loss_.dtype)
+    loss_ *= mask
+
+    return tf.reduce_mean(loss_)
+
+
+def train_RNN(epochs, start_epoch, ckpt_manager, num_steps,
+              dataset, decoder, encoder, word_to_index):
+    EPOCHS = epochs
+    total_time = 0
+    for epoch in range(start_epoch, EPOCHS):
+        start = time.time()
+        total_loss = 0
+
+        for (batch, (img_tensor, target)) in enumerate(dataset):
+            batch_loss, t_loss = train_step(
+                img_tensor, target, decoder, encoder, word_to_index)
+            total_loss += t_loss
+
+            if batch % 100 == 0:
+                average_batch_loss = batch_loss.numpy()/int(target.shape[1])
+                print(
+                    f'Epoch {epoch+1} Batch {batch} Loss {average_batch_loss:.4f}')
+        # storing the epoch end loss value to plot later
+        loss_plot.append(total_loss / num_steps)
+
+        if epoch % 5 == 0:
+            ckpt_manager.save()
+
+        print(f'Epoch {epoch+1} Loss {total_loss/num_steps:.6f}')
+        print(f'Time taken for 1 epoch {time.time()-start:.2f} sec\n')
+        total_time += time.time()-start
+
+    print(f'Total time taken: {(total_time/60):.2f} min\n')
---
+++ models/utilities.py
@@ -0,0 +1,92 @@
+import os
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import json
+import collections
+
+
+def import_files():
+    # Download caption annotation files
+    annotation_file = 'dataset\coco\\tarin\captions_train2017.json'
+    annotation_folder = '\dataset\coco\\annotations_trainval2017\\annotations\\'
+
+    # Download image files
+    image_folder = '\dataset\coco\\tarin\images\\'
+    PATH = os.path.abspath('.') + image_folder
+
+    with open(annotation_file, 'r') as f:
+        annotations = json.load(f)
+# Group all captions together having the same image ID.
+    image_path_to_caption = collections.defaultdict(list)
+    for val in annotations['annotations']:
+        caption = f"<start> {val['caption']} <end>"
+        image_path = PATH + '%012d.jpg' % (val['image_id'])
+        # image_path.replace("\\","/") #windows
+        image_path_to_caption[image_path].append(caption)
+
+    return image_path_to_caption
+
+
+def load_image(image_path):
+    img = tf.io.read_file(image_path)
+    img = tf.io.decode_jpeg(img, channels=3)
+    img = tf.keras.layers.Resizing(224, 224)(img)
+    img = tf.keras.applications.resnet50.preprocess_input(img)
+    return img, image_path
+
+
+def plot_attention(image, result, attention_plot):
+    temp_image = np.array(Image.open(image))
+
+    fig = plt.figure(figsize=(50, 50))
+
+    len_result = len(result)
+    for i in range(len_result):
+        temp_att = np.resize(attention_plot[i], (8, 8))
+        grid_size = max(int(np.ceil(len_result/2)), 2)
+        ax = fig.add_subplot(grid_size, grid_size, i+1)
+        ax.set_title(result[i])
+        img = ax.imshow(temp_image)
+        ax.imshow(temp_att, cmap='gray', alpha=0.6, extent=img.get_extent())
+
+    plt.tight_layout()
+    plt.show()
+
+
+def evaluate(image, max_length, attention_features_shape, encoder, decoder,
+             image_features_extract_model, word_to_index, index_to_word):
+    attention_plot = np.zeros((max_length, attention_features_shape))
+
+    hidden = decoder.reset_state(batch_size=1)
+
+    temp_input = tf.expand_dims(load_image(image)[0], 0)
+
+    img_tensor_val = image_features_extract_model(temp_input)
+
+    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0],
+                                                 -1,
+                                                 img_tensor_val.shape[3]))
+    features = encoder(img_tensor_val)
+    dec_input = tf.expand_dims([word_to_index('<start>')], 0)
+    result = []
+
+    for i in range(max_length):
+        predictions, hidden, attention_weights = decoder(dec_input,
+                                                         features,
+                                                         hidden)
+
+        attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
+
+        predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+        predicted_word = tf.compat.as_text(index_to_word(predicted_id).numpy())
+        result.append(predicted_word)
+
+        if predicted_word == '<end>':
+            return result, attention_plot
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    attention_plot = attention_plot[:len(result), :]
+    return result, attention_plot
