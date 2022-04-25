8c32820b06de14345e05450efedabd4cae488dec
Hazem <hazemkya@gmail.com>
2022-04-25 14:12:58 +0300

    Done training

--- train.py
+++ train.py
@@ -25,6 +25,41 @@ embedding_dim = int(config['config']['embedding_dim'])
 # Training parameters
 epochs = int(config['config']['epochs'])
 num_steps = len(img_name_train) // BATCH_SIZE
+embeddings_index = {}
+
+if use_glove:
+    glove_path = "./dataset/glove.6B/glove.6B.50d.txt"
+
+    
+    with open(glove_path, encoding="utf8") as f:
+        for line in f:
+            word, coefs = line.split(maxsplit=1)
+            coefs = np.fromstring(coefs, "f", sep=" ")
+            embeddings_index[word] = coefs
+
+    print("Found %s word vectors." % len(embeddings_index))
+
+    vocabulary = tokenizer.get_vocabulary()
+    word_index = dict(zip(vocabulary, range(len(vocabulary))))
+
+    num_tokens = len(vocabulary) + 2
+    embedding_dim = 50
+    hits = 0
+    misses = 0
+
+    # Prepare embedding matrix
+    embedding_matrix = np.zeros((num_tokens, embedding_dim))
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # Words not found in embedding index will be all-zeros.
+            # This includes the representation for "padding" and "OOV"
+            embedding_matrix[i] = embedding_vector
+            hits += 1
+        else:
+            misses += 1
+    print("Converted %d words (%d misses)" % (hits, misses))
+
 
 encoder = CNN_Encoder(embedding_dim)
 if use_glove:
