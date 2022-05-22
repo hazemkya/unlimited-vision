91482f1a0e67da98851dd0e5b245d7a30d905c9d
Hazem <hazemkya@gmail.com>
2022-05-22 20:41:52 +0300

    Bug fixes

--- models/train_utils.py
+++ models/train_utils.py
@@ -21,6 +21,7 @@ config.read("config.ini")
 save_path = config["config"]["save_path"]
 early_stop = int(config["config"]["early_stop"])
 eval_percentage = float(config["config"]["eval_percentage"])
+use_early_stop = False
 
 
 @tf.function
@@ -104,29 +105,35 @@ def train(epochs, start_epoch, ckpt_manager,
         print(f'Epoch {epoch+1} Loss {float(total_loss/num_steps)}',
               "last save: ", float(best_score))
 
-        bleu_curr = evaluate_epoch(img_name_vector_val, encoder, decoder,
-                                   image_features_extract_model, word_to_index_train,
-                                   index_to_word_train, best_score)
+        if len(loss_plot) % 4 == 0:
+            bleu_curr = evaluate_epoch(img_name_vector_val, encoder, decoder,
+                                       image_features_extract_model, word_to_index_train,
+                                       index_to_word_train, best_score)
+
+        # save a checkpoint if the loss is better than the last saved loss
+        if use_early_stop:
+            if (bleu_curr >= best_score):
+                no_change_since = 0
+                ckpt_manager.save()
+                save_loss(loss_plot)
+                print("Chekpoint autosave current: ",
+                      float(bleu_curr), "last save: ", float(best_score))
+                best_score = bleu_curr
+            else:
+                no_change_since += 1
+                print(f"No loss gained since {no_change_since}")
+
+            if no_change_since >= early_stop:
+                print(
+                    f"Ending training because no loss has been gained for {no_change_since} epochs.")
+                break
+        else:
+            ckpt_manager.save()
+            save_loss(loss_plot)
+            print("Chekpoint autosave current")
 
         print(f'Time taken for 1 epoch {time.time()-start:.2f} sec\n')
         total_time += time.time()-start
-
-        # save a checkpoint if the loss is better than the last saved loss
-        if (bleu_curr >= best_score):
-            no_change_since = 0
-            ckpt_manager.save()
-            save_loss(loss_plot)
-            print("Chekpoint autosave current: ",
-                  float(bleu_curr), "last save: ", float(best_score))
-            best_score = bleu_curr
-        else:
-            no_change_since += 1
-            print(f"No loss gained since {no_change_since}")
-
-        if no_change_since >= early_stop:
-            print(
-                f"Ending training because no loss has been gained for {no_change_since} epochs.")
-            break
 
     print(f'Total time taken: {(total_time/60):.2f} min\n')
 
@@ -155,7 +162,7 @@ def evaluate_epoch(img_name_vector_val, encoder, decoder,
         coco_eval.evaluate()
     print("================================")
     for metric, score in coco_eval.eval.items():
-        if metric == 'Bleu_1':
+        if metric == 'SPICE':
             bleu_curr = score
         print(f'{metric}: {score}')
 
